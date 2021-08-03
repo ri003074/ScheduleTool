0921d518d119afbfe5b16a586be2f3baf3caa0d2
kenta <ri003074@gmail.com>
2021-08-03 20:49:26 +0900

    add venv

--- win32com/main.py
+++ win32com/main.py
@@ -1,17 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import win32com.client
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, I am {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("Hello")
+def sample():
+    xl = win32com.client.GetObject(Class="Excel.Application")
+    wb = xl.Workbooks("demo_01.xlsx")
+    ws = wb.Sheets(1)
+    ws.Range("A1").Value = 123
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    sample()
