a78a651b4a2ab7b33facf495d5f23b2eb4dbe70b
kenta <ri003074@gmail.com>
2021-08-04 06:21:24 +0900

    small update

--- win32com/main.py
+++ win32com/main.py
@@ -1,11 +1,14 @@
 import win32com.client
+import sys
+
+sys.dont_write_bytecode = True
 
 
 def sample():
     xl = win32com.client.GetObject(Class="Excel.Application")
     wb = xl.Workbooks("demo_01.xlsx")
     ws = wb.Sheets(1)
-    ws.Range("A1").Value = 123
+    ws.Range(" A1").Value = 123
 
 
 if __name__ == "__main__":
