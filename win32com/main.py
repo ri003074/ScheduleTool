8982a491c6ea89e9f0b6d5851085efff555d3c9d
kenta <ri003074@gmail.com>
2021-08-07 11:16:43 +0900

    update

--- win32com/main.py
+++ win32com/main.py
@@ -1,16 +1,7 @@
-import win32com.client
-import sys
+from table import assign_task
+
 from table import create_table
 
-
-def sample():
-    xl = win32com.client.GetObject(Class="Excel.Application")
-    wb = xl.Workbooks("demo_01.xlsx")
-    ws = wb.Sheets(1)
-    ws.Range("A1").Value = 123
-
-
 if __name__ == "__main__":
-    sys.dont_write_bytecode = 1
-    sample()
     create_table()
+    assign_task()
---
+++ win32com/variables.py
@@ -0,0 +1,5 @@
+xlHAlignCenter = -4108
+xlUp = -4162
+xlDown = -4121
+xlToLeft = -4159
+xlToRight = -4161
